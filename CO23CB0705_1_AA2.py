1bc2b20cbcf4ac78774800cb9615a5ac6371e35d
UoloLearning <127477826+UoloLearning@users.noreply.github.com>
2023-05-22 17:25:44 +0530

    Update and rename CO23CB0705_1_AA2 to CO23CB0705_1_AA2.py

---
+++ CO23CB0705_1_AA2.py
@@ -0,0 +1,14 @@
+fruit_rates = {
+    "apple": 80,       # per kilogram
+    "banana": 40,      # per dozen
+    "orange": 60,      # per kilogram
+    "mango": 70,      # per kilogram
+}
+apple_rate = fruit_rates["apple"]
+print("The rate of apples is:", apple_rate, "INR per kilogram")
+banana_rate = fruit_rates["banana"]
+print("The rate of bananas is:", banana_rate, "INR per dozen")
+orange_rate = fruit_rates["orange"]
+print("The rate of orange is:", orange_rate, "INR per kilogram")
+mango_rate = fruit_rates["mango"]
+print("The rate of mango is:", mango_rate, "INR per kilogram")
