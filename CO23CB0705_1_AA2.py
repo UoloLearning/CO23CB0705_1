3eff7d28edd1b50b18dc342d819b93c30e1fc093
UoloLearning <127477826+UoloLearning@users.noreply.github.com>
2023-05-22 18:08:51 +0530

    Update CO23CB0705_1_AA2.py

--- CO23CB0705_1_AA2.py
+++ CO23CB0705_1_AA2.py
@@ -1,14 +1,14 @@
-fruit_rates = {
+fruit_prices = {
     "apple": 80,       # per kilogram
     "banana": 40,      # per dozen
     "orange": 60,      # per kilogram
     "mango": 70,      # per kilogram
 }
-apple_rate = fruit_rates["apple"]
+apple_rate = fruit_prices["apple"]
 print("The rate of apples is:", apple_rate, "INR per kilogram")
-banana_rate = fruit_rates["banana"]
+banana_rate = fruit_prices["banana"]
 print("The rate of bananas is:", banana_rate, "INR per dozen")
-orange_rate = fruit_rates["orange"]
+orange_rate = fruit_prices["orange"]
 print("The rate of orange is:", orange_rate, "INR per kilogram")
-mango_rate = fruit_rates["mango"]
+mango_rate = fruit_prices["mango"]
 print("The rate of mango is:", mango_rate, "INR per kilogram")
