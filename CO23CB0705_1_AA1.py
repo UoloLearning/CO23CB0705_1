67283bd40715ea3545708831e57dffd927a59ee5
UoloLearning <127477826+UoloLearning@users.noreply.github.com>
2023-05-22 17:08:41 +0530

    Update and rename CO23CB0705_1_AA1 to CO23CB0705_1_AA1.py

---
+++ CO23CB0705_1_AA1.py
@@ -0,0 +1,10 @@
+dictionary = {
+    "name": "Conji",
+    "age": 13,
+    "city": "Avora"
+}
+
+# Accessing an integer value from the dictionary
+age = dictionary["age"]
+
+print(f"The age is: {age}")
